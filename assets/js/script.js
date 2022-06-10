e8eb1dd166ec1ea906aed24f86444e3baa59ff18
MosNes <7hekarl@gmail.com>
2022-06-09 20:42:26 -0400

    added and tested function for generating authrozation token for spotify

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,47 @@
 //spotify secret credentials
-var client_id = 'CLIENT_ID';
-var client_secret = 'CLIENT_SECRET';
+var client_id = '';
+var client_secret = '';
+
+//get auth button for testing
+var getAuthButton = $("#get-auth-token-btn");
 
 //get spotify access token
 var getSpotifyAcessToken = function () {
     //object that contains the info to make the request
-    var authOptions = {
-        url: 'https://accounts.spotify.com/api/token',
+ 
+    fetch('https://accounts.spotify.com/api/token', {
+        method: 'POST',
         headers: {
-            'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+            //btoa encodes the string into base64
+            'Authorization': 'Basic ' + btoa(client_id+':'+client_secret),
+            'Content-Type': 'application/x-www-form-urlencoded'
         },
-        form: {
-            grant_type: 'client_credentials'
-        },
-        json: true
-    };
-
-    request.post(authOptions, function (error, response, body) {
-        //if there is no error and status code is 200, return an access token
-        if (!error && response.statusCode === 200) {
-            var token = body.access_token;
-        }
-        console.log(token);
+        body: 'grant_type=client_credentials',
+    })
+    .then((response) => {return response.json()})
+    .then((response)=>{
+        var token = response.access_token;
+        localStorage.setItem('spotifyTempToken',token);
+        $("#spotify-response").text("Authorized");
     });
 };
+
+
+
+
+
+
+    
+
+
+//     request.post(authOptions, function (error, response, body) {
+//         //if there is no error and status code is 200, return an access token
+//         if (!error && response.statusCode === 200) {
+//             var token = body.access_token;
+//         }
+//         console.log(token);
+//     });
+// };
+
+
+getAuthButton.on("click", getSpotifyAcessToken);
