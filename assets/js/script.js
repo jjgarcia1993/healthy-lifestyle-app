30374631a6c68bffcaafa6dda7afb7e41652c816
MosNes <7hekarl@gmail.com>
2022-06-13 14:28:21 -0400

    added HTML and styling for search bar and Spotify Connection notification

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
 //--------------GLOBAL VARIABLES--------------------------
 
 //spotify secret credentials
-const client_id = '';
-const client_secret = '';
+const client_id = '0cb998c652e247539b3e6bb2e2c5cb93';
+const client_secret = 'ee5dae716eb44c48b3d235b1cf35fbab';
 
 //get auth button for testing
 var getAuthButton = $("#get-auth-token-btn");
@@ -27,12 +27,27 @@ var getSpotifyAcessToken = function () {
         },
         body: 'grant_type=client_credentials',
     })
-    .then((response) => {return response.json()})
-    .then((response)=>{
-        var token = response.access_token;
-        localStorage.setItem('spotifyTempToken',token);
-        $("#spotify-response").text("Authorized");
-    });
+    .then((response) => {
+        if (response.ok){
+            return response.json()
+            .then((response)=>{
+                var token = response.access_token;
+                localStorage.setItem('spotifyTempToken',token);
+                $("#spotify-response").append($("<div>")
+                    .addClass("notification is-success")
+                    .text("Connected to Spotify")
+                    .append($("<button>")
+                        .addClass("delete")));
+            });
+        }else{
+           $("#spotify-response").append($("<div>")
+                    .addClass("notification is-warning")
+                    .text("Failed to connect to Spotify")
+                    .append($("<button>")
+                        .addClass("delete")));;
+            return false;
+        }
+    })  
 };
 
 //test API call
@@ -44,7 +59,14 @@ var testApiCall = function(){
             'Content-Type':'application/json'
         }
     })
-    .then((response) => {return response.json()})
+    .then((response) => {
+        if (response.ok){
+            return response.json();
+        }else{
+            window.alert("Unable to communicate with Spotify.")
+            return false;
+        }  
+    })
     .then((response) => {
         console.log(response);
     });
@@ -53,6 +75,10 @@ var testApiCall = function(){
 
 //---------------INITIALIZATIONS------------------
 
+//gets the spotify access token and displays a notification if successful
+getSpotifyAcessToken();
 
-getAuthButton.on("click", getSpotifyAcessToken);
-testApiButton.on("click", testApiCall);
+//allows the user to click the "X" button with a class of .delete to remove the element
+$("body").on("click", ".delete", function(){
+    $(this).parent().remove();
+});
