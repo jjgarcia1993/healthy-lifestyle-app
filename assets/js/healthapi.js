048ace2d20b412a2908e2fc7cdd1496e3c7638b8
Jonathan Garcia <102456485+jjgarcia1993@users.noreply.github.com>
2022-06-14 18:37:09 -0400

    initial files

---
+++ assets/js/healthapi.js
@@ -0,0 +1,12 @@
+
+
+
+    const access = '5c9ae3d61310402e9ddb0d502d29573d7f612ac2'
+
+fetch('https://wger.de/en/exercise/overview/')
+    .then(function(response) {
+        return response.json()
+    })
+    .then(function (data) {
+        console.log('the date', data)
+    })
