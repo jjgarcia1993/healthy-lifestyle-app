cd39aa0ea0698eed95cf20af054947815a0b1265
Jonathan Garcia <102456485+jjgarcia1993@users.noreply.github.com>
2022-06-14 19:21:36 -0400

    exercise category pull

--- assets/js/healthapi.js
+++ assets/js/healthapi.js
@@ -1,10 +1,7 @@
+const access = '5c9ae3d61310402e9ddb0d502d29573d7f612ac2'
 
-
-
-    const access = '5c9ae3d61310402e9ddb0d502d29573d7f612ac2'
-
-fetch('https://wger.de/en/exercise/overview/')
-    .then(function(response) {
+fetch('https://wger.de/api/v2/exercisecategory/')
+    .then(function (response) {
         return response.json()
     })
     .then(function (data) {
