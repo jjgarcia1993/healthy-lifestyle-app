ca607370776daa30636696c31fcf7d67a2326189
Jonathan Garcia <102456485+jjgarcia1993@users.noreply.github.com>
2022-06-14 20:14:46 -0400

    mess

--- assets/js/healthapi.js
+++ assets/js/healthapi.js
@@ -1,9 +1,29 @@
+
+
 const access = '5c9ae3d61310402e9ddb0d502d29573d7f612ac2'
 
-fetch('https://wger.de/api/v2/exercisecategory/')
+fetch('https://wger.de/api/v2/exercise?category=12&language=2')
     .then(function (response) {
         return response.json()
     })
     .then(function (data) {
         console.log('the date', data)
     })
+
+
+//creates a card with the playlist info and appends it to the spotifyCardContainer
+var createPlaylistCard = (exerciseObj) => {
+    let cardEl = $("<div>").addClass("column is-one-quarter card m-2");
+    let deleteEl = $("<button>").addClass("delete");
+    let cardHeaderEl = $("<header>").addClass("card-header")
+        .append($("<p>").addClass("card-header-title").text(exerciseObj.name));
+    let cardContentEl = $("<div>").addClass("card-content");
+    let descriptionEl = $("<p>").addClass("content").text(exerciseObj.description);
+    let cardFooterEl = $("<footer>").addClass("card-footer");
+    let playlistLinkEl = $("<a>").addClass("card-footer-item").attr("href", 'https://wger.de/en/exercise/'+ exerciseObj.id +'/view/').text("Link");
+
+    cardFooterEl.append(playlistLinkEl);
+    cardContentEl.append(descriptionEl);
+    cardEl.append(deleteEl, cardHeaderEl, cardContentEl, cardFooterEl);
+
+};
