74e07e7675e344645325ace2e554b1ae66d217eb
Jonathan Garcia <102456485+jjgarcia1993@users.noreply.github.com>
2022-06-14 20:24:28 -0400

    added exercise search function

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,39 +12,39 @@ const spotifyCardContainerEl = $("#card-container");
 
 //get spotify access token
 var getSpotifyAcessToken = function () {
-    
-    
+
+
     //NEED TO ADD ERROR HANDLING FOR THIS TOKEN REQUEST
     fetch('https://accounts.spotify.com/api/token', {
         method: 'POST',
         headers: {
             //btoa encodes the string into base64
-            'Authorization': 'Basic ' + btoa(client_id+':'+client_secret),
+            'Authorization': 'Basic ' + btoa(client_id + ':' + client_secret),
             'Content-Type': 'application/x-www-form-urlencoded'
         },
         body: 'grant_type=client_credentials',
     })
-    .then((response) => {
-        if (response.ok){
-            return response.json()
-            .then((response)=>{
-                var token = response.access_token;
-                localStorage.setItem('spotifyTempToken',token);
+        .then((response) => {
+            if (response.ok) {
+                return response.json()
+                    .then((response) => {
+                        var token = response.access_token;
+                        localStorage.setItem('spotifyTempToken', token);
+                        $("#spotify-response").append($("<div>")
+                            .addClass("notification is-success")
+                            .text("Connected to Spotify")
+                            .append($("<button>")
+                                .addClass("delete")));
+                    });
+            } else {
                 $("#spotify-response").append($("<div>")
-                    .addClass("notification is-success")
-                    .text("Connected to Spotify")
-                    .append($("<button>")
-                        .addClass("delete")));
-            });
-        }else{
-           $("#spotify-response").append($("<div>")
                     .addClass("notification is-warning")
                     .text("Failed to connect to Spotify")
                     .append($("<button>")
                         .addClass("delete")));;
-            return false;
-        }
-    })  
+                return false;
+            }
+        })
 };
 
 
@@ -57,7 +57,7 @@ var createPlaylistCard = (playlistObj) => {
     let cardContentEl = $("<div>").addClass("card-content");
     let descriptionEl = $("<p>").addClass("content").text(playlistObj.description);
     let cardFooterEl = $("<footer>").addClass("card-footer");
-    let playlistLinkEl = $("<a>").addClass("card-footer-item").attr("href",playlistObj.external_urls.spotify).text("Link");
+    let playlistLinkEl = $("<a>").addClass("card-footer-item").attr("href", playlistObj.external_urls.spotify).text("Link");
 
     cardFooterEl.append(playlistLinkEl);
     cardContentEl.append(descriptionEl);
@@ -66,29 +66,63 @@ var createPlaylistCard = (playlistObj) => {
 
 };
 
+
+
+
+
+// Exercise card 
+var createPlaylistCard = (exerciseObj) => {
+    let cardEl = $("<div>").addClass("column is-one-quarter card m-2");
+    let deleteEl = $("<button>").addClass("delete");
+    let cardHeaderEl = $("<header>").addClass("card-header")
+        .append($("<p>").addClass("card-header-title").text(exerciseObj.name));
+    let cardContentEl = $("<div>").addClass("card-content");
+    let descriptionEl = $("<p>").addClass("content").text(exerciseObj.description);
+    let cardFooterEl = $("<footer>").addClass("card-footer");
+    let playlistLinkEl = $("<a>").addClass("card-footer-item").attr("href", 'https://wger.de/en/exercise/' + exerciseObj.id + '/view/').text("Link");
+
+    cardFooterEl.append(playlistLinkEl);
+    cardContentEl.append(descriptionEl);
+    cardEl.append(deleteEl, cardHeaderEl, cardContentEl, cardFooterEl);
+
+};
+
+// Search for exercise
+var searchExercise = function (searchInput) {
+    fetch('https://wger.de/api/v2/exercise?category='+searchInput.id+'&language=2')
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (data) {
+            console.log('the date', data)
+        })
+};
+
+
+
 //search for playlists
-var searchPlaylists = function(searchInput){
-    fetch('https://api.spotify.com/v1/search?q='+searchInput+'&type=playlist',{
+var searchPlaylists = function (searchInput) {
+    fetch('https://api.spotify.com/v1/search?q=' + searchInput + '&type=playlist', {
         method: 'GET',
         headers: {
-            'Authorization': 'Bearer '+localStorage.getItem('spotifyTempToken'),
-            'Content-Type':'application/json'
+            'Authorization': 'Bearer ' + localStorage.getItem('spotifyTempToken'),
+            'Content-Type': 'application/json'
         }
     })
-    .then((response) => {
-        if (response.ok){
-            return response.json();
-        }else{
-            window.alert("Unable to communicate with Spotify.")
-            return false;
-        }  
-    })
-    .then((data) => {
-        let playlists = data.playlists.items;
-        for (let i = 0; i < 6; i++){
-            createPlaylistCard(playlists[i]);
-        }
-    });
+        .then((response) => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                window.alert("Unable to communicate with Spotify.")
+                return false;
+            }
+        })
+        .then((data) => {
+            let playlists = data.playlists.items;
+            for (let i = 0; i < 6; i++) {
+                createPlaylistCard(playlists[i]);
+            }
+        });
 };
 
 
@@ -108,9 +142,9 @@ var clearAllCards = () => {
 getSpotifyAcessToken();
 
 //allows the user to click the "X" button with a class of .delete to remove the element
-$("body").on("click", ".delete", function(){
+$("body").on("click", ".delete", function () {
     $(this).parent().remove();
 });
 
-$("#search-button").on("click",searchHandler);
-$("#clear-button").on("click",clearAllCards);
+$("#search-button").on("click", searchHandler);
+$("#clear-button").on("click", clearAllCards);
