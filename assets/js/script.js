a5d808fe6b3803a2fe48181dd8983524e6fa9575
MosNes <7hekarl@gmail.com>
2022-06-15 22:58:28 -0400

    tied both API search functions together, modified page layout, styled result cards, added header generators for both card sections, added data validation to some form inputs

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,8 @@ const client_id = '0cb998c652e247539b3e6bb2e2c5cb93';
 const client_secret = 'ee5dae716eb44c48b3d235b1cf35fbab';
 
 //Container Elements
-const spotifyCardContainerEl = $("#card-container");
+const spotifyCardContainerEl = $("#spotify-card-container");
+const workoutContainerEl = $("#workout-card-container")
 
 
 //----------------FUNCTIONS----------------------
@@ -53,11 +54,11 @@ var createPlaylistCard = (playlistObj) => {
     let cardEl = $("<div>").addClass("column is-one-quarter card m-2");
     let deleteEl = $("<button>").addClass("delete");
     let cardHeaderEl = $("<header>").addClass("card-header")
-        .append($("<p>").addClass("card-header-title").text(playlistObj.name));
+        .append($("<p>").addClass("card-header-title is-size-4").text(playlistObj.name));
     let cardContentEl = $("<div>").addClass("card-content");
     let descriptionEl = $("<p>").addClass("content").text(playlistObj.description);
     let cardFooterEl = $("<footer>").addClass("card-footer");
-    let playlistLinkEl = $("<a>").addClass("card-footer-item").attr("href", playlistObj.external_urls.spotify).text("Link");
+    let playlistLinkEl = $("<a>").addClass("card-footer-item is-size-4").attr("href", playlistObj.external_urls.spotify).text("Link");
 
     cardFooterEl.append(playlistLinkEl);
     cardContentEl.append(descriptionEl);
@@ -72,24 +73,24 @@ var createPlaylistCard = (playlistObj) => {
 
 // Exercise card 
 var createExerciseCard = (exerciseObj) => {
-    let cardEl = $("<div>").addClass("column is-one-quarter card m-2");
+    let cardEl = $("<div>").addClass("column card m-2");
     let deleteEl = $("<button>").addClass("delete");
     let cardHeaderEl = $("<header>").addClass("card-header")
-        .append($("<p>").addClass("card-header-title").text(exerciseObj.name));
+        .append($("<p>").addClass("card-header-title is-size-4").text(exerciseObj.name));
     let cardContentEl = $("<div>").addClass("card-content");
     let descriptionEl = $("<p>").addClass("content").html(exerciseObj.description);
     let cardFooterEl = $("<footer>").addClass("card-footer");
-    let playlistLinkEl = $("<a>").addClass("card-footer-item").attr("href", 'https://wger.de/en/exercise/' + exerciseObj.id + '/view/').text("Link");
+    let workoutLinkEl = $("<a>").addClass("card-footer-item is-size-4").attr("href", 'https://wger.de/en/exercise/' + exerciseObj.id + '/view/').text("Link");
 
-    cardFooterEl.append(playlistLinkEl);
+    cardFooterEl.append(workoutLinkEl);
     cardContentEl.append(descriptionEl);
     cardEl.append(deleteEl, cardHeaderEl, cardContentEl, cardFooterEl);
-    spotifyCardContainerEl.append(cardEl);
+    workoutContainerEl.append(cardEl);
 };
 
 // Search for exercise
 var searchExercise = function (searchInput) {
-    fetch('https://wger.de/api/v2/exercise?category=' + searchInput.id + '&language=2')
+    fetch('https://wger.de/api/v2/exercise?category=' + searchInput + '&language=2')
         .then(function (response) {
             return response.json()
         })
@@ -106,7 +107,7 @@ var searchExercise = function (searchInput) {
 
 //search for playlists
 var searchPlaylists = function (searchInput) {
-    fetch('https://api.spotify.com/v1/search?q=' + searchInput + '&type=playlist', {
+    fetch('https://api.spotify.com/v1/search?q=workout%20' + searchInput + '&type=playlist', {
         method: 'GET',
         headers: {
             'Authorization': 'Bearer ' + localStorage.getItem('spotifyTempToken'),
@@ -131,13 +132,60 @@ var searchPlaylists = function (searchInput) {
 
 
 var searchHandler = () => {
-    $("#card-container").empty();
-    let searchInput = $("#spotify-search-input").val();
-    searchPlaylists(searchInput);
+    spotifyCardContainerEl.empty();
+    workoutContainerEl.empty();
+    $("#your-playlist-header").empty();
+    $("#your-workout-header").empty();
+
+    //get user's name from the input field
+    let userName = $("#user-name").val();
+
+    //data validation for user's name
+    if(!userName){
+        //shows warning notification
+        $("#search-warning-container").append($("<div>")
+        .addClass("notification is-warning block")
+        .text("Please enter your name.")
+        .append($("<button>")
+            .addClass("delete")));
+        return false;
+    }
+
+    //gets data attributes from whichever radio button is currently "checked"
+    let workoutId = $("input[name='workout-type']:checked").attr("data-id");
+    let playlistInput = $("input[name='workout-type']:checked").attr("data-name");
+
+    //data validation for radio buttons
+    if (!workoutId || !playlistInput) {
+        //shows warning notification
+        $("#search-warning-container").append($("<div>")
+                            .addClass("notification is-warning block")
+                            .text("Please select a workout.")
+                            .append($("<button>")
+                                .addClass("delete")));
+        return false;
+    }
+
+    //adds title to playlist section
+    $("#your-playlist-header").append(
+        $("<h2>").addClass("is-size-2 block").text("Playlists for "+ userName)
+    );
+
+     //adds title to workout section
+     $("#your-workout-header").append(
+        $("<h2>").addClass("is-size-2 block").text("Workouts for "+ userName)
+    );
+
+    //runs the wger search
+    searchExercise(workoutId);
+
+    //runs the spotify search
+    searchPlaylists(playlistInput);
 };
 
 var clearAllCards = () => {
-    $("#card-container").empty();
+   spotifyCardContainerEl.empty();
+   workoutContainerEl.empty();
 };
 
 //---------------INITIALIZATIONS------------------
@@ -150,5 +198,5 @@ $("body").on("click", ".delete", function () {
     $(this).parent().remove();
 });
 
-$("#search-button").on("click", searchHandler);
+$("#show-results-btn").on("click", searchHandler);
 $("#clear-button").on("click", clearAllCards);
